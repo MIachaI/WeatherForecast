bb1f8521f38cc3d269228547b6a3351d63aa1b78
MIachaI <mprawda@wp.pl>
2017-05-22 10:54:19 +0200

    Api is currently working, add more variables and put it into table with icons

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,8 +13,19 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-
-        return $this->render('default/index.html.twig', [
+        $weather_data=file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&appid=ee13ce11b5ce9bc77e5e7c8856232a34');
+        $json=json_decode($weather_data, TRUE);
+        $dlugosc_geograficzna=$json['coord']['lon'];
+        $szerokosc_geograficzna=$json['coord']['lat'];
+        $temp=$json['main']['temp'];
+        $pressure=$json['main']['pressure'];
+        $wilgotnosc=$json['main']['humidity'];
+        return $this->render('default/index.html.twig', ['pogoda'=>$weather_data,
+            'dlugosc'=>$dlugosc_geograficzna,
+            'szerokosc'=>$szerokosc_geograficzna,
+            'temperatura'=>$temp,
+            'cisnienie'=>$pressure,
+            'wilgotnosc'=>$wilgotnosc
         ]);
     }
 }
